1bc836b857b233188b79ea03db54f820808fdd7d
Sydney Crumley <srsimps19@gmail.com>
2019-05-15 18:19:55 -0500

    more styling

--- friends/src/Components/FriendCard.js
+++ friends/src/Components/FriendCard.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CardDiv = styled.div`
-width: 500px
+width: 900px
 display: flex
+justify-content: center
 flex-wrap: wrap`;
 
 function FriendCard(props) {
@@ -11,9 +12,9 @@ function FriendCard(props) {
         <CardDiv>
             {props.friends.map(friend => (
                 <div className="friendCard" key={friend.id}>
-                    <h4>{friend.name}</h4>
-                    <p>{friend.age}</p>
-                    <p>{friend.email}</p>
+                    <h4>Name: {friend.name}</h4>
+                    <p>Age: {friend.age}</p>
+                    <p>Email: {friend.email}</p>
                     <button onClick={props.deleteFriend}>Delete Friend</button>
                 </div>
             ))}
